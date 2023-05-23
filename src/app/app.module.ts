1e314d19616a56f679301d23a9d7849855a9ca65
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-05-23 16:14:50 +0300

    completed sign up design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LoginCardComponent } from './login-card/login-card.component';
 import { SignupComponent } from './signup/signup.component';
+import { SignupCardComponent } from './signup-card/signup-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     LoginCardComponent,
-    SignupComponent
+    SignupComponent,
+    SignupCardComponent
   ],
   imports: [
     BrowserModule,
