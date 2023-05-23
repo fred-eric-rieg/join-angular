64b05b3fe8f2a8bed1681592509621405439b2b8
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-05-23 18:56:44 +0300

    united components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { LoginCardComponent } from './login-card/login-card.component';
 import { SignupComponent } from './signup/signup.component';
-import { SignupCardComponent } from './signup-card/signup-card.component';
 import { ForgotPwComponent } from './forgot-pw/forgot-pw.component';
-import { ForgotPwCardComponent } from './forgot-pw-card/forgot-pw-card.component';
 import { ResetPwComponent } from './reset-pw/reset-pw.component';
-import { ResetPwCardComponent } from './reset-pw-card/reset-pw-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    LoginCardComponent,
     SignupComponent,
-    SignupCardComponent,
     ForgotPwComponent,
-    ForgotPwCardComponent,
-    ResetPwComponent,
-    ResetPwCardComponent
+    ResetPwComponent
   ],
   imports: [
     BrowserModule,
