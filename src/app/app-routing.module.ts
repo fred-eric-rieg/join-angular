a13dbbdabfb3cfd5d3f13ad1f784978895eb14ca
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-05-23 18:59:42 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent} from './signup/signup.component';
 import { ForgotPwComponent } from './forgot-pw/forgot-pw.component';
 import { ResetPwComponent } from './reset-pw/reset-pw.component';
+import { SummaryComponent } from './summary/summary.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'forgot-pw', component: ForgotPwComponent },
-  { path: 'reset-pw', component: ResetPwComponent}
+  { path: 'reset-pw', component: ResetPwComponent },
+  { path: 'summary', component: SummaryComponent}
 ];
 
 @NgModule({
