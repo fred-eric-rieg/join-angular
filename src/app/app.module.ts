6db3c3dd81c6fe6a553056ecb5026b9443e165d8
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-05-23 15:59:45 +0300

    created signup component and routingLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LoginCardComponent } from './login-card/login-card.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    LoginCardComponent
+    LoginCardComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
