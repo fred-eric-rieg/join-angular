7ee2d90f76b03f917b7c8b9389bd69ba72f40365
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-05-23 18:58:53 +0300

    created summary component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ForgotPwComponent } from './forgot-pw/forgot-pw.component';
 import { ResetPwComponent } from './reset-pw/reset-pw.component';
+import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ResetPwComponent } from './reset-pw/reset-pw.component';
     LoginComponent,
     SignupComponent,
     ForgotPwComponent,
-    ResetPwComponent
+    ResetPwComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
