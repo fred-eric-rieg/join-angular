ea05735198ac5c0c9e158d52944d357181faae1e
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-05-25 08:56:43 +0300

    created contacts component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BoardComponent } from './board/board.component';
 import { AddtaskComponent } from './addtask/addtask.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { AddtaskComponent } from './addtask/addtask.component';
     HeaderComponent,
     FooterComponent,
     BoardComponent,
-    AddtaskComponent
+    AddtaskComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
