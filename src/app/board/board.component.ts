e3ab0569eed2102eb9dedca10de5b022b0b62c46
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-05-31 17:38:47 +0300

    board render function

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit {
 
+  tasks: object = [];
+
+  constructor(private firebaseService: FirebaseService) { }
+
+  
+  ngOnInit() {
+    this.tasks = this.firebaseService.getAllTasks();
+    this.renderTasks();
+  }
+
+
+  renderTasks() {
+  }
 }
