4dbf7dba72fb25e338105fda8d31bfae8e36beb6
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-06-09 09:20:13 +0300

    implemented firebaseService for summary

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Task } from 'src/app/models/task.class';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-summary',
@@ -10,29 +12,48 @@ export class SummaryComponent implements OnInit {
   username: string = 'Sofia Müller';
   @ViewChild('greeting') greeting: any;
   hasGreeted: boolean = false;
+  tasks!: Task;
 
-  constructor() {
-    
+  
+  constructor(private firebaseService: FirebaseService) {
+    this.firebaseService.tasks.subscribe(tasks => {
+      this.tasks = tasks;
+    });
   }
+
 
   ngOnInit() {
     this.getLocalStorage();
     if (!this.hasGreeted) {
-      setTimeout(() => {
-        this.hasGreeted = true;
-        this.setLocalStorage();
-        this.greeting.nativeElement.style.transition = 'all 1s ease-out';
-        this.greeting.nativeElement.style.opacity = 0;
-        this.greeting.nativeElement.style.zIndex = -1;
-      }, 3000);
+      this.fadeOutGreeting();
     }
   }
 
+  /**
+   * Fades out greeting after 3 seconds and saves the hasGreeted boolean into localStorage.
+   * This way, the greeting will not be shown again. Only after user logs out, then hasGreeted will
+   * be reset to "false".
+   */
+  fadeOutGreeting() {
+    setTimeout(() => {
+      this.hasGreeted = true;
+      this.setLocalStorage();
+      this.greeting.nativeElement.style.transition = 'all 1s ease-out';
+      this.greeting.nativeElement.style.opacity = 0;
+      this.greeting.nativeElement.style.zIndex = -1;
+    }, 3000);
+  }
 
+  /**
+   * Saving the hasGreeted boolean in the localStorage.
+   */
   setLocalStorage() {
     localStorage.setItem('hasGreeted', this.hasGreeted.toString());
   }
 
+  /**
+   * Loading the hasGreeted boolean from the localStorage.
+   */
   getLocalStorage() {
     if (localStorage.getItem('hasGreeted')) {
       this.hasGreeted = localStorage.getItem('hasGreeted') === 'true' ? true : false;
