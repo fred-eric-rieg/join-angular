f35f618f8fbd16baf2fca492dfe24d452e7f8347
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-06-11 09:03:11 +0300

    cleaned code in addtask

--- src/app/components/addtask/addtask.component.ts
+++ src/app/components/addtask/addtask.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FirebaseService } from '../../services/firebase.service';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-addtask',
@@ -16,6 +16,7 @@ export class AddtaskComponent {
   mediumPrio: boolean = false;
   urgentPrio: boolean = false;
 
+  // Toggle variables for input fields.
   expandedCategory: boolean = false;
   expandedAssigned: boolean = false;
 
@@ -34,18 +35,29 @@ export class AddtaskComponent {
   subtasks: any = [];
   subtask: string = '';
 
+  // Temporary storage for fetched data from Firestore
   categories: any = [];
   users: any = [];
 
+  // The form group data collection
   taskForm!: FormGroup;
 
 
-  constructor(private firebaseService: FirebaseService, private formBuilder: FormBuilder, private router: Router) {
-
-  }
+  constructor(private firebaseService: FirebaseService, private formBuilder: FormBuilder, private router: Router) { }
 
 
   ngOnInit() {
+    this.setTaskFormGroup();
+    this.subscribeToCategories();
+    this.subscribeToUsers();
+    this.taskForm.valueChanges.subscribe();
+  }
+
+
+  /**
+   * Initial setup for the taskForm group with values and validators.
+   */
+  setTaskFormGroup() {
     this.taskForm = this.formBuilder.group({
       id: '',
       title: ['', [Validators.required]],
@@ -60,38 +72,73 @@ export class AddtaskComponent {
       assignedTo: [[], [Validators.required]],
       subtasks: [[]]
     });
+  }
+
+
+  /**
+   * Observing changes on categories and copying them into categories variable.
+   */
+  subscribeToCategories() {
     this.firebaseService.categories.subscribe(categories => {
       categories.forEach((category: any) => {
         this.categories.push(category);
       });
     });
+  }
+
+
+  /**
+   * Observing changes on users and copying them into users variable.
+   */
+  subscribeToUsers() {
     this.firebaseService.users.subscribe(users => {
       users.forEach((user: any) => {
         this.users.push(user);
       });
     });
-    this.taskForm.valueChanges.subscribe(console.log);
   }
 
 
+  /**
+   * Sets priority in taskForm group depending on which priority-button the user clicked.
+   * @param prio as string
+   */
   setPrio(prio: string) {
     if (prio === 'low') {
-      this.lowPrio = true;
-      this.mediumPrio = false;
-      this.urgentPrio = false;
+      this.setLowPrio();
     } else if (prio === 'medium') {
-      this.lowPrio = false;
-      this.mediumPrio = true;
-      this.urgentPrio = false;
+      this.setMediumPrio();
     } else if (prio === 'high') {
-      this.lowPrio = false;
-      this.mediumPrio = false;
-      this.urgentPrio = true;
+      this.setHighPrio();
     }
     this.taskForm.patchValue({ priority: prio });
   }
 
 
+  setLowPrio() {
+    this.lowPrio = true;
+    this.mediumPrio = false;
+    this.urgentPrio = false;
+  }
+
+
+  setMediumPrio() {
+    this.lowPrio = false;
+    this.mediumPrio = true;
+    this.urgentPrio = false;
+  }
+
+
+  setHighPrio() {
+    this.lowPrio = false;
+    this.mediumPrio = false;
+    this.urgentPrio = true;
+  }
+
+
+  /**
+   * Toggle expansion of input field "category".
+   */
   expandCategory() {
     if (this.expandedCategory) {
       this.expandedCategory = false;
@@ -101,6 +148,10 @@ export class AddtaskComponent {
   }
 
 
+  /**
+   * Stores the user-selected category in the variable selectedCategory.
+   * @param index as number
+   */
   selectCategory(index: number) {
     this.selectedCategory = [this.categories[index].name, this.categories[index].color];
     this.taskForm.patchValue({ category: this.selectedCategory });
@@ -133,6 +184,9 @@ export class AddtaskComponent {
   }
 
 
+  /**
+   * Setting selectedCategory's values to newCategory and newColor and updating firestore accordingly.
+   */
   createNewCategory() {
     if (this.newCategory) {
       this.selectedCategory = [this.newCategory, this.newColor];
@@ -145,6 +199,9 @@ export class AddtaskComponent {
   }
 
 
+  /**
+   * Toggle expansion of input field "assign to".
+   */
   expandAssigned() {
     if (this.expandedAssigned) {
       this.expandedAssigned = false;
@@ -154,21 +211,41 @@ export class AddtaskComponent {
   }
 
 
+  /**
+   * Add or remove a user from the assignedUsers array via the userId.
+   * Updating taskForm accordingly.
+   * @param userId as string
+   */
   toggleAssignment(userId: string) {
     if (this.isAssigned(userId)) {
-      this.assignedUsers.splice(this.assignedUsers.findIndex((user: any) => user.id === userId), 1);
-      this.taskForm.patchValue({ assignedTo: this.assignedUsers });
+      this.removeUserAndUpdateTaskForm(userId);
     } else {
-      this.users.forEach((user: any) => {
-        if (user.userId === userId) {
-          this.assignedUsers.push({ id: user.userId, name: user.firstName + ' ' + user.lastName, color: user.color });
-          this.taskForm.patchValue({ assignedTo: this.assignedUsers });
-        }
-      });
+      this.assignUserAndUpdateTaskForm(userId);
     }
   }
 
 
+  removeUserAndUpdateTaskForm(userId: string) {
+    this.assignedUsers.splice(this.assignedUsers.findIndex((user: any) => user.id === userId), 1);
+    this.taskForm.patchValue({ assignedTo: this.assignedUsers });
+  }
+
+
+  assignUserAndUpdateTaskForm(userId: string) {
+    this.users.forEach((user: any) => {
+      if (user.userId === userId) {
+        this.assignedUsers.push({ id: user.userId, name: user.firstName + ' ' + user.lastName, color: user.color });
+        this.taskForm.patchValue({ assignedTo: this.assignedUsers });
+      }
+    });
+  }
+
+
+  /**
+   * Checks, if a user is listed in the assignedUsers array via the userId.
+   * @param userId as string
+   * @returns true or false
+   */
   isAssigned(userId: string) {
     let assigned = false;
     this.assignedUsers.forEach((user: any) => {
@@ -213,7 +290,6 @@ export class AddtaskComponent {
 
   createTask() {
     if (this.taskForm.valid) {
-      console.log(this.taskForm.value);
       this.firebaseService.createTask(this.taskForm.value);
       this.router.navigate(['board']);
     } else {
