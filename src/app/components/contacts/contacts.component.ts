5a0ec0eb06d8b55e4e843ea8d211fa98af94ef5e
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-06-12 09:43:38 +0300

    implementing firebaseService for contacts

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
-export class ContactsComponent {
+export class ContactsComponent implements OnInit {
+
+  users!: any[];
+
+
+  constructor(private firebaseService: FirebaseService) { }
+
+
+  ngOnInit(): void {
+    this.firebaseService.users.subscribe(users => {
+      this.users = users;
+    });
+  }
 
 }
