4c0347330b170f29ea67f70c8aa57684b68ec1ca
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-06-12 13:58:31 +0300

    created contact form inputs & design

--- src/app/components/contact-add/contact-add.component.ts
+++ src/app/components/contact-add/contact-add.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -10,11 +11,24 @@ export class ContactAddComponent {
 
   type: string = '';
 
-  constructor(private route: ActivatedRoute) { }
+  contactForm!: FormGroup;
+
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createFormGroup();
     this.route.params.subscribe(params => {
       this.type = params['id'];
+    });
+  }
+
+
+  createFormGroup() {
+    this.contactForm = this.formBuilder.group({
+      id: new FormControl('id'),
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [Validators.required, Validators.pattern("^[0-9]*$")])
     });
   }
 }
