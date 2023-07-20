524f00da80ed05fd3388284a92f15e24a919760d
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-07-20 20:52:15 +0300

    implemented signup && validation

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +9,44 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  form!: FormGroup;
+  // loading boolean for the signup button: prevents user from clicking multiple times
+  loading: boolean = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AngularFireAuth
+    ) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.minLength(6), Validators.required]],
+      passwordRepeat: ['', []]
+    });
+  }
+
+
+  async signup() {
+    if (this.passwordMatch()) {
+      this.loading = true;
+      this.authService.createUserWithEmailAndPassword(this.form.value.email, this.form.value.password).then((user) => {
+        localStorage.setItem('user', JSON.stringify(user));
+        localStorage.setItem('hasGreeted', 'false');
+        window.location.href = '/summary';
+      }).catch((error) => {
+        alert(error.message);
+      });
+    } else {
+      alert('Passwords do not match!');
+    }
+    this.loading = false;
+  }
+
+
+  passwordMatch() {
+    if (this.form.value.password === this.form.value.passwordRepeat) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
