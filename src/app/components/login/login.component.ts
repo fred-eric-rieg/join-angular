aca0bad6973395be13f4c922710dce93b591816d
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-07-20 19:28:09 +0300

    implemented FireAuth login and form validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,30 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {}
+  form!: FormGroup;
+  minLengthPassword: number = 6;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AngularFireAuth
+    ) {}
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
     localStorage.clear();
+  }
+
+
+  async login(email: string, password: string) {
+    return await this.authService.signInWithEmailAndPassword(email, password).then((user) => {
+      localStorage.setItem('user', JSON.stringify(user));
+      localStorage.setItem('hasGreeted', 'false');
+      window.location.href = '/summary';
+    }).catch((error) => {
+      alert(error.message);
+    });
   }
 }
